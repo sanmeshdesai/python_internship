ef97a4fc9cc7235c75f5043d364b7d8afdd7bd4e
sanmesh <sanmeshdesai11@gmail.com>
2026-01-18 22:38:55 +0530

    data structure - tuple

---
+++ notes/list-tuple.md
@@ -0,0 +1,11 @@
+Aspect	List	Tuple
+Mutability	Mutable (elements can be changed)	Immutable (elements cannot be changed)
+Memory Allocation	Uses more memory	Uses less memory
+Performance	Slower for iteration	Faster for iteration
+Hashability	❌ Not hashable	✅ Hashable (if elements are hashable)
+Thread Safety	Not thread-safe	Thread-safe (due to immutability)
+Use as Dictionary Key	❌ Not allowed	✅ Allowed
+Built-in Methods	Many methods (append, pop, remove)	Limited methods (count, index)
+Data Integrity	Can be modified accidentally	Data remains constant
+Memory Optimization	Less efficient	More efficient
+Use in Functional Programming	Rare	Preferred
---
+++ tuple.py
@@ -0,0 +1,79 @@
+# tuple: oredered collection of simmiler or diff type of immutable data.
+# syntax ()
+# faster - fixed size length.
+
+# skill = ('HTML','CSS','JS','Python')
+# print(skill)
+
+# print(skill[2])
+
+# skill[2] = 'tailwind' //error
+
+# emp = 'html', 'css', 'js'
+# print(emp)
+
+# print(type(emp))
+
+# looping
+# users = ('john','motu','patlu','ninja','shiva','aditya','shinchan')
+# # for i in users:
+# #     print(i)
+
+
+# for i in range(2, 6):
+#     print(users[i])
+
+# built in function
+# t2 = (20,23,25,27,21)
+# print(min(t2))
+# print(max(t2))
+# print(sum(t2))
+
+
+# print(20 in t2)
+
+
+# print(sorted(t2))
+# print(sorted(t2, reverse=True))
+
+
+# t1 = [12,33432,532,656,243,1]
+# print(sorted(t1))
+
+
+# packing and unpacking
+
+
+# brands = ('Samsung','Lenovo','Apple','Toshiba','Xiaomi','Realme')
+
+# brand1,brand2,brand3,brand4,brand5,brand6 = brands
+# print(brand1)
+
+
+# l1 = [10, 30, 40, 50]
+# print(type(l1))
+# print(tuple(l1))
+
+
+# a = ('apple','banana', 'apple')
+# print(a.count('apple'))
+
+
+
+# number = (10, 20, 30)
+# n1, n2, n3 = number
+# print(n1)
+# print(n2)
+# print(n3)
+
+
+# t = (10, (100, 200, 300, 400), 30, 40)
+# print(t[1][2])
+
+
+# colors = ("Red", "Green", "Blue", "Yellow", "Purple")
+
+# for i in colors:
+#     print(i)
+
+
