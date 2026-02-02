0021a235a4085739916b1794fffee9e57e18f40d
sanmesh <sanmeshdesai11@gmail.com>
2026-02-03 00:00:04 +0530

    polymorphism

---
+++ methodoverloading.py
@@ -0,0 +1,12 @@
+# compile time polymorphism
+# method overloading
+# polymorphism with different methods in the same class 
+# doees not support method overloading directly like some other languages
+
+class Calculator:
+
+    def add(self, a, b, c=0):
+        return a + b + c
+    
+
+print(str(10), '20')
---
+++ polymorphism.py
@@ -0,0 +1,62 @@
+# one method different forms
+# based on the object, arguments, classes etc.
+
+# run time polymorphism
+# method overriding
+# polymorphism with inheritance
+
+class Parent:
+    def Hello(self):
+        print("Hello from Parent class")
+
+class Child(Parent):    
+    def Hello(self):
+        super().Hello()
+        print("Hello from Child class")
+
+
+c1 = Child()
+c1.Hello()  # Hello from Child class
+
+
+class PaymentGateway:
+
+    def __init__(self, balance, orderId, customerId, status):
+        self.balance = balance
+        self.orderId = orderId
+        self.customerId = customerId
+        self.status = status
+
+    def details(self):
+        print(f'Balance: {self.balance}, Order ID: {self.orderId}, Customer ID: {self.customerId}, Status: {self.status}')
+
+
+
+
+class UPI(PaymentGateway):
+
+    
+    def pay(self):
+        print("Paying through UPI")
+
+    def details(self):
+        print("UPI Payment Details:")
+        super().details()
+
+class COD(PaymentGateway):
+    def pa
+
+    
+    def pay(self):
+        print("Paying through Cash on Delivery")
+
+    def details(self):
+        print("COD Payment Details:")
+        super().details()
+
+u = UPI(100, 'ORD123', 'CUST456', 'Completed')
+u.pay()
+u.details()
+
+
+
